e521acde7a06c97a345b298042909bbab036a03d
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-12 09:32:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ async def increment_view_count(session):
     try:
         async with session.get(url) as response:
             if response.status == 200:
-                print("\033[92m[\033[1m+\033[92m]\033[31mSentPING: " +str(u))+ "\033[96mRequest-status " +url+ "\033[0m" )
+                print("\033[92m[\033[1m+\033[92m]\033[31mSentPING: " +str(u)+ "\033[96mRequest-status " +url+ "\033[0m" )
             else:
                 print("Failed ping.")
     except aiohttp.ClientError as e:
