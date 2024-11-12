d38f17b592cad1162cc84cbde7569fddbabd0adf
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-12 10:05:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def increment_view_count(session):
             else:
                 print("Failed ping.")
         except aiohttp.ClientError as e:
-                print("\033[92m[\033[1m+\033[92mAn error occurred:\033[0m", e)
+                print("\033[92m[+\033[92m] An error occurred:\033[0m", e)
 
 async def main():
     connector = aiohttp.TCPConnector(limit=None) # Enable connection pooling
