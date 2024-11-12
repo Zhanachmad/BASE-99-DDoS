3cd7869a6d3756c9f36982fd785438bfc5514d6b
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-12 18:58:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,48 +13,6 @@ if os.name == 'nt':
 else:
     os.system('clear')
 
-# Colorclass bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    ZA1 = '\033[31m'
-    ZA2 = '\033[32m'
-    ZA3 = '\033[33m'
-    FAIL = '\033[91m'
-    RESET = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    ZH = '\033[97m'
-os.system("clear")
-print(" ") 
-print("\033[33m      ® ® ® ® @      @ @ @        @ @ @   @ @ @ @ @   \033[0m")
-print("\033[33m      ®        ®   @       @    @         @           \033[0m")
-print("\033[33m      ®        ®  @         @   @         @           \033[0m")
-print("\033[33m      ®        ®  @         @   @         @           \033[0m")
-print("\033[96m      ®        ®  @         @   @         @           \033[0m")
-print("\033[96m      ®  @ @ ®    @         @   @         @ © @ @     \033[0m")
-print("\033[96m      ®        @  @ @ @ @ @ @     @ @ @   @           \033[0m")
-print("\033[91m      ®        ®  @         ®          @  @           \033[0m")
-print("\033[91m      ®        ®  @         @          @  @           \033[0m")
-print("\033[91m      ® ® @ @ @   @         @    @ @ @    @ @ @ @ @   \033[0m")
-print("\033[91m                                                      \033[0m")
-print("\033[33m                                      @ @      @ @    \033[0m")
-print("\033[33m                                    @     @  @     @  \033[0m")
-print("\033[33m                                    @     @  @     @  \033[0m")
-print("\033[92m                                    @     @  @     @  \033[0m")
-print("\033[92m                                      @ @ @    @ @ @  \033[0m") 
-print("\033[92m                                          @        @  \033[0m") 
-print("\033[92m                                       @ @     @ @    \033[0m") 
-print(" ")                                                 
-print("\033[33m⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵\033[0m")
-print("\033[33m⁵            BRIGADE  ATTACKER  SNIPER ELITE                  ⁵\033[0m")
-print("\033[33m⁵                                                             ⁵\033[0m")
-print("\033[33m⁵                    Design By Za'99                          ⁵\033[0m")
-print("\033[33m⁵                                                             ⁵\033[0m")
-print("\033[33m⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵~ BIRRUH BIDDAM NAFDIKA YA AQSHA ~⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵⁵\033[0m")
-print("\033[96m----------------------------------------------------------\033[0m")
 url = input("\033[92mIP/URL: \033[0m").strip()
 url = input("\033[92Press Enter u/ melanjutkan:\033[0m")
 time.sleep(5),
