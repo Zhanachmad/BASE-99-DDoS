418c7e632f9368e9c80813e573bd5df220640988
Kafulkaff99 <kafulkaff02@gmail.com>
2024-11-12 11:54:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ if os.name == 'nt':
 else:
     os.system('clear')
 
-
 # Colorclass bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -59,29 +58,29 @@ print("\033[96m----------------------------------------------------------\033[0m
 url = input("\033[92mIP/URL: \033[0m").strip()
 url = input("\033[92Press Enter u/ melanjutkan:\033[0m")
 time.sleep(5),
-print("\033[92m... 1 \033[0m "),
+print("\033[92m                               1 \033[0m "),
 time.sleep(5),
-print("\033[1m..... 2 \033[0m "),
+print("\033[1m                               2 \033[0m "),
 time.sleep(5),
-print("\033[97m...... 3 \033[0m "),
+print("\033[97m                               3 \033[0m "),
 time.sleep(5),
-print("\033[95m........ 4 \033[0m "),
+print("\033[95m                               4 \033[0m "),
 time.sleep(5),
-print("\033[92m........... 5 \033[0m "),
+print("\033[92m                               5 \033[0m "),
 async def increment_view_count(session):
     try:
         async with session.get(url) as response:
             if response.status == 200:
-                print("\033[92m[\033[1m+\033[92m]\033[31mSent\033[96mRequest\033[0m" )
+                print("\033[92m[+\033[92m]\033[31mSent\033[96mRequest\033[0m")
             if response.status == 200:
-                print("\033[92m[\033[1m+\033[92m]\033[31mSent\033[96m Request\033[0m" )
+                print("\033[92m[+\033[92m]\033[31mSent\033[96m Request\033[0m")
             if response.status == 200:
-                print("\033[92m[\033[1m+\033[92m]\033[31mSen\033[96m  Request\033[0m" )
+                print("\033[92m[+\033[92m]\033[31mSen\033[96m   Request\033[0m")
             
             else:
                 print("Failed ping.")
     except aiohttp.ClientError as e:
-        print("\033[92m[\033[1m+\033[92mAn error occurred:\033[0m", e)
+                print("\033[92m[+\033[92m] \033[1mAn error occurred:\033[0m", e)
 
 async def main():
     connector = aiohttp.TCPConnector(limit=None) # Enable connection pooling
